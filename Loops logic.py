5d72fee17e68c0f4381e777b25ce9f814299f3fb
Ramsha Anwar <ramshaanwar.work@gmail.com>
2026-01-23 00:44:44 +0500

    Add practice Loops logic.py

---
+++ Loops logic.py
@@ -0,0 +1,73 @@
+### Questions:###
+
+# Q.1: LOOPS concept:
+# Print natural number upto n
+# for i in range(1,n): srf n likhogi tw desire number se km hi aega q k woh last se pehle +1 kr k ruk jata hai
+
+# n = int(input("Please put your lucky number here"))
+# for i in range(1,n+1):
+#  print(i)
+
+# ___________________
+
+# Q.2: LOOPS concept:
+# Resverse for loop . print n to 1
+# reverse loop m shuru main n and hmesha -1 use krenge end m, or jaha tk ulta le jana hai whaa se pehle wala number likhenge
+
+# n = int(input("Please put your lucky number here"))
+
+# for i in range(n,0,-1):
+#  print(i)
+
+# ___________________
+
+# Q.3: LOOPS concept:
+# Take a number as input and print its table
+
+# Basic Concept:
+
+# n = int(input("Which numbers table you want?"))
+# for i in range(n,(n*10)+1,n):
+#     print(i)
+
+# Good level concept:
+
+# n = int(input("Which numbers table you want?"))
+
+# for i in range(1,11):
+#     print(f"{n} * {i} = {n*i}")
+
+# ___________________
+
+# Q.4: LOOPS concept:
+# Sum upto n terms
+
+# a = 1
+# a = a + 2
+# a = a + 3
+# a = a + 4
+
+# print (a) #10
+
+# n = int(input("Enter the number for sum"))
+# sum = 0
+
+# for i in range (1,n+1):
+#     sum = sum + i
+
+# print(sum)
+
+# ___________________
+
+# Q.5: LOOPS concept:
+# Factorial of a number
+
+# n = int(input("Please tell your number"))
+# fact = 1 #0 is liye nhi rkhenge wrna sb 0 hojaega
+
+# for i in range(1,n+1):
+#     fact = fact * i
+
+# print(fact)
+# ___________________
+
