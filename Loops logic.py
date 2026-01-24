0cc66fa14592e73f0dca2dec00f23a05407faa1b
Ramsha Anwar <ramshaanwar.work@gmail.com>
2026-01-24 23:59:55 +0500

    Add loops logic practice

--- Loops logic.py
+++ Loops logic.py
@@ -63,6 +63,7 @@
 # Factorial of a number
 
 # n = int(input("Please tell your number"))
+
 # fact = 1 #0 is liye nhi rkhenge wrna sb 0 hojaega
 
 # for i in range(1,n+1):
@@ -71,3 +72,121 @@
 # print(fact)
 # ___________________
 
+
+# Q.6: LOOPS Advance concept:
+# prime numbers woh hote h jo apne ables m aate h baki composite number - factors woh hote h jo dyusrou k tables m bhi aate hai, jese  12 ab woh 1,2,3,4,6,12 in sb k tables m ataa hai. 
+# logic m reminder 0 hoga tw factor warna prime
+# Print all the factors of a number.
+
+# n = int(input("Please give your number"))
+
+# for i in range (1,n+1):
+#     if n%i == 0:
+#      print(i, end = " ") #iss end = " " ek line m ajaege next pr shift nhi houge
+# ___________________
+
+# Q.7: LOOPS Advance concept:
+# Accept number and check it if perfect or not
+# A number whose sum of factors(excluding number itself) = Number Ex - 6 = 1, 2 , 3 = 6
+# sare factors jo nikle hai, apna number chrh k un sb ko sum krna hai
+
+# n = int(input("Tell your number"))
+
+# sum = 0
+# for i in range (1,n):
+#     if n%i == 0:
+#        sum = sum + i
+
+# print(sum)
+
+# if we tell my number is strong number like perfect jis ka sum bhi original number ho us k liu=ye ye krenge.
+
+# if sum == n:
+#     print ("This is a perfect number")
+# else:
+#     print("This is not a perfect number")
+
+# ___________________
+
+# Q.8: LOOPS Advance concept:
+# Check if the number is prime or not
+
+# n = int(input("Tell your number"))
+
+# count = 0
+# for i in range (1,n+1):
+#     if n%i == 0:
+#         count = count + 1
+# if count == 2:
+#     print("This is prime number")
+# else:
+#     print("This is composite number")
+
+# ___________________
+
+# Q.9: LOOPS Advance concept:
+# Separate each digit of a number and print it on the new line Ex - 123
+# Mode division % apne last digit ko separate krdeta hai, flow division// apne last digit ko gyb krdeta hai
+
+# a = 123
+# print(a%10)
+# a = a//10
+# print(a%10)
+# a = a//10
+# print(a%10)
+# a = a//10
+# print(a%10)
+
+# Condition k jaha pr zero ajega waha ruk jan hai tw hm while use krenge
+
+# a = int(input("Tell your number"))
+
+# while a > 0:
+#     print(a%10)
+#     a = a//10
+
+# ___________________
+
+# Q.10: LOOPS Advance concept:
+# Accept number and check if it is a pallindromic number
+# (if number and its reverse are equal)
+# Ex - 12321 - Reverse - 12321
+
+# n = int(input("Tell your number")) #123
+# rev = 0
+# while n > 0:
+#     z = n%10
+#     rev = rev*10 + z
+#     n = n//10
+
+# print (rev)
+
+# abhi kahani khtmnhi hoi hai ye srf reverse arha hai, hme donou ko same bhi check krna hai.
+
+# n = int(input("Tell your number")) #123
+# copy = n
+# rev = 0
+# while n > 0:
+#     z = n%10
+#     rev = rev*10 + z
+#     n = n//10
+
+# if copy == rev:
+#     print("PALLINDROMIC NUMBER")
+# else:
+#     print("Not PALLINDROMIC NUMBER")
+
+# ___________________
+
+# Q.11: LOOPS Advance concept:
+# (Strong Number)
+# Ek number Strong Number tab hota hai jab uske har ek digit ka factorial nikaal kar unhe jama (add) kiya jaye, aur answer wahi original number aaye.
+
+a = int(input("Tell your number"))
+
+temp = a
+
+while a > 0:
+    digit = a % 10
+
+    
